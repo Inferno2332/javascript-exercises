fcaf93e8a769a60eb6ec74941fb58ef923d5b825
Inferno2332 <yucharter3@gmail.com>
2025-03-22 18:34:48 +1100

    complete 03 to 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    if (a<0 || b<0) {
+        return 'ERROR';
+    }
+    let n = Math.max(a,b)-Math.min(a,b)+1;
+    return n*(a+b)/2;
 };
 
 // Do not edit below this line
